a249c4c74b8e0b4bd3614a871e2a1791d73ddb75
Tokyobiso <tokyo.biso.tutorial@gmail.com>
2024-01-11 07:05:12 +0900

    Bing対応 2024.01.11

--- src/features/chat/chat-services/Azure-bing-search/bing.ts
+++ src/features/chat/chat-services/Azure-bing-search/bing.ts
@@ -1,13 +1,13 @@
 export class BingSearchResult {
   async  SearchWeb(searchText: string)  {
-    var url = "https://api.bing.microsoft.com/v7.0/search?count=10&offset=0&responseFilter=Webpages&setLang=ja-jp&q=";
+    var url : string  = "https://api.bing.microsoft.com/v7.0/search?count=10&offset=0&responseFilter=Webpages&setLang=ja-jp&q=";
     url = url + searchText;
     try {
       const response = await fetch(url, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
-          'Ocp-Apim-Subscription-Key': ''//process.env.AZURE_BINGSEARCH_API_KEY
+          'Ocp-Apim-Subscription-Key': process.env.AZURE_BINGSEARCH_API_KEY
           }});
       return await response.json();
       //const postsData = await response.json();
